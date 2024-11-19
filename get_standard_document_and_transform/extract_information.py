f04357464df58f507ad0b0b31e2076d3c1fc2d7b
happypildo <yoonpd.workspace@gmail.com>
2024-11-19 16:46:38 +0900

    PDF에서 title / release / area 추출 완료 -> 이후 목차/스코프/결론 부분 추출

---
+++ get_standard_document_and_transform/extract_information.py
@@ -0,0 +1,55 @@
+import json
+import os
+
+import fitz
+import re
+
+import glob
+
+
+class Extractor:
+    def __init__(self, target_path):
+        self.extracted_data = []
+        self.target_path = target_path
+
+    def extract_content_from_pdf(self):
+        pdf_files = glob.glob(f"{self.target_path}/*.pdf")
+
+        for filename in pdf_files:
+            file_path = os.path.abspath(f"./{filename}")
+
+            pdf = fitz.open(file_path)
+
+            first_page = pdf.load_page(0)
+
+            try:
+                content = first_page.get_text()
+                content = content.replace("\n", "")
+                content = content.replace(";", " ")
+                content = content.split("Technical Specification Group Radio Access Network")
+
+                standard_title = content[0][:content[0].index("(")]
+                study_area = content[1][:content[1].index("(Release")]
+
+                release_s = content[1].index("(Release")
+                release_e = content[1].index("The present document")
+                release = content[1][release_s + 1:release_e]
+
+                standard_title = standard_title.strip()
+                study_area = study_area.strip()
+                release = release.strip()[:-1]
+
+                print("-"*10)
+                print(standard_title)
+                print(study_area)
+                print(release)
+                print()
+
+            except:
+                pass
+
+    def extract_information_from_tr(self, pdf):
+        pass
+
+    def extract_information_from_ts(self, pdf):
+        pass
