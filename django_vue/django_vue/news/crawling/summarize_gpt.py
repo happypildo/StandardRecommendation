57c7dc6db646bcecea4649408f8a8fddac49c840
happypildo <yoonpd.workspace@gmail.com>
2024-11-21 16:46:12 +0900

    News 기사 리스팅 및 요약 기능 추가 - 이후 키워드 추출 과정 추가 예정

--- django_vue/django_vue/news/crawling/summarize_gpt.py
+++ django_vue/django_vue/news/crawling/summarize_gpt.py
@@ -56,7 +56,7 @@ class Summarizer:
         ]
 
     def summarize_news(self, title, summarized_content, difficulty='일반인'):
-        return f"제목: {title}\n요청 요약 수준: {difficulty}\n기사 요약: {summarized_content}."
+        return f"{summarized_content}."
 
     def chat_completion_request(self, messages):
         try:
@@ -98,29 +98,29 @@ class Summarizer:
         return "요약에 실패했습니다."
 
 
-summa = Summarizer()
-message = summa.use_chat_gpt_for_summarization(
-    title="5G Non-Terrestrial Networks",
-    content="""Just as the work in 3GPP is starting on Rel-19 Non-Terrestrial Networks (NTN), five leading contributors have released a new book on 5G NTN and beyond. Alessandro Vanelli-Coralli, Nicolas Chuberre, Gino Masini, Alessandro Guidotti, Mohamed El Jaafari are the authors of "5G Non-Terrestrial Networks: Technologies, Standards, and System Design" (Wiley-IEEE Press).
-Drawing from their expertise in their respective fields, the authors have covered recent developments in 3GPP Releases and at the definition of new work in Rel-19. The book covers different aspects of NTN including NR, NB-IoT & eMTC principles, use cases and system architecture.
-In Rel-19, the following NR and IoT areas are particularly important:
-Non-Terrestrial Networks (NTN) for NR Phase 3 (NR_NTN_Ph3)
-In Rel-17 and Rel-18 solutions enabling New Radio and NG-RAN to support Non-Terrestrial Networks (NTN) were defined.  As part of Rel-19, a new work item has been approved defining further improvements for:
-Optimized performance for terminals.
-Capacity performance on uplink.
-Notification of the service area of a Broadcast service.
-Support for a non-terrestrial network architecture with 5G system functions on board the NTN vehicle (i.e. regenerative payloads).
-Use of RedCap devices within FR1 NTN.
-Non-Terrestrial Networks (NTN) for Internet of Things (IoT) Phase 3
-With IoT-NTN specified in 3GPP RAN Rel-17, with optimizations following in Rel-18, commercial deployments are now ongoing. Now, further evolution of IoT-NTN is underway with a dedicated Rel-19 work item, focusing in on three areas:
-Support of Store & Forward (S&F) operation based on regenerative payload, including the support of feeder link switchover.
-Uplink capacity enhancements.
-The book also includes a comprehensive introduction to NTN, with the elements of a satellite communications system, orbits and constellations, technologies and challenges, related industrial projects, potential future developments, and others.
-According to Gino Masini, one of the five coauthors of 5G Non-Terrestrial Networks, "Working on this book while actively contributing in 3GPP was a big challenge for all of us, but I am confident that such a first-hand source of information will ultimately benefit the readers."
-The group of authors is representative of :the various parts of the 3GPP community, spanning different industries and academia: Alessandro Vanelli-Coralli and Alessandro Guidotti are Professors with the University of Bologna, Gino Masini (former 3GPP RAN3 Chair) is with Ericsson, and Nicolas Chuberre (NTN WI Rapporteur) and Mohamed El Jaafari are with Thales Alenia Space. All of them are active as standards delegates for their respective organizations, both within 3GPP WGs and TSGs  and outside the project.
-The book is available from Wiley-IEEE Press.
-Some background reading:
-Non-Terrestrial Networks (NTN) for NR Phase 3 (NR_NTN_Ph3, RP-234078)
-Non-Terrestrial Networks (NTN) for Internet of Things (IoT) Phase 3 (IoT_NTN_Ph3, RP-234077)"""
-)
-print(message)
+# summa = Summarizer()
+# message = summa.use_chat_gpt_for_summarization(
+#     title="5G Non-Terrestrial Networks",
+#     content="""Just as the work in 3GPP is starting on Rel-19 Non-Terrestrial Networks (NTN), five leading contributors have released a new book on 5G NTN and beyond. Alessandro Vanelli-Coralli, Nicolas Chuberre, Gino Masini, Alessandro Guidotti, Mohamed El Jaafari are the authors of "5G Non-Terrestrial Networks: Technologies, Standards, and System Design" (Wiley-IEEE Press).
+# Drawing from their expertise in their respective fields, the authors have covered recent developments in 3GPP Releases and at the definition of new work in Rel-19. The book covers different aspects of NTN including NR, NB-IoT & eMTC principles, use cases and system architecture.
+# In Rel-19, the following NR and IoT areas are particularly important:
+# Non-Terrestrial Networks (NTN) for NR Phase 3 (NR_NTN_Ph3)
+# In Rel-17 and Rel-18 solutions enabling New Radio and NG-RAN to support Non-Terrestrial Networks (NTN) were defined.  As part of Rel-19, a new work item has been approved defining further improvements for:
+# Optimized performance for terminals.
+# Capacity performance on uplink.
+# Notification of the service area of a Broadcast service.
+# Support for a non-terrestrial network architecture with 5G system functions on board the NTN vehicle (i.e. regenerative payloads).
+# Use of RedCap devices within FR1 NTN.
+# Non-Terrestrial Networks (NTN) for Internet of Things (IoT) Phase 3
+# With IoT-NTN specified in 3GPP RAN Rel-17, with optimizations following in Rel-18, commercial deployments are now ongoing. Now, further evolution of IoT-NTN is underway with a dedicated Rel-19 work item, focusing in on three areas:
+# Support of Store & Forward (S&F) operation based on regenerative payload, including the support of feeder link switchover.
+# Uplink capacity enhancements.
+# The book also includes a comprehensive introduction to NTN, with the elements of a satellite communications system, orbits and constellations, technologies and challenges, related industrial projects, potential future developments, and others.
+# According to Gino Masini, one of the five coauthors of 5G Non-Terrestrial Networks, "Working on this book while actively contributing in 3GPP was a big challenge for all of us, but I am confident that such a first-hand source of information will ultimately benefit the readers."
+# The group of authors is representative of :the various parts of the 3GPP community, spanning different industries and academia: Alessandro Vanelli-Coralli and Alessandro Guidotti are Professors with the University of Bologna, Gino Masini (former 3GPP RAN3 Chair) is with Ericsson, and Nicolas Chuberre (NTN WI Rapporteur) and Mohamed El Jaafari are with Thales Alenia Space. All of them are active as standards delegates for their respective organizations, both within 3GPP WGs and TSGs  and outside the project.
+# The book is available from Wiley-IEEE Press.
+# Some background reading:
+# Non-Terrestrial Networks (NTN) for NR Phase 3 (NR_NTN_Ph3, RP-234078)
+# Non-Terrestrial Networks (NTN) for Internet of Things (IoT) Phase 3 (IoT_NTN_Ph3, RP-234077)"""
+# )
+# print(message)
