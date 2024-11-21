9dc9ad64eb71997f11dcfc7e4d672397f353eb75
happypildo <yoonpd.workspace@gmail.com>
2024-11-21 14:38:32 +0900

    GPT 요약 탑재시키는 중

--- django_vue/django_vue/news/views.py
+++ django_vue/django_vue/news/views.py
@@ -7,11 +7,14 @@ from django.http import JsonResponse, HttpResponse
 from .models import News
 
 from .crawling.news_data_crawling import Crawler
+from .crawling.summarize_gpt import Summarizer
+
 
 # 게시글 생성(POST), 전체 게시글 조회(GET)
 @api_view(['GET'])
 def news_list(request):
     crawler = Crawler()
+    summarizer = Summarizer()
 
     for news in crawler.crawl_data():
         print("In the view.py -------------------------------------")
@@ -21,5 +24,14 @@ def news_list(request):
                 title=news['title'],
                 content=news['contents']
             )
+        message = summarizer.use_chat_gpt_for_summarization(
+            title=news['title'],
+            content=news['contents']
+        )
+        print("Summarization: ")
+        print(message)
+        
+
+        
     
     return JsonResponse()
